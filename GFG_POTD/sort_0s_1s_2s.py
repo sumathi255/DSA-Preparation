3b1df5a0a8e023ca634611c42171c605d02b867f
sumathi255 <adimulamsumathi432@gmail.com>
2026-01-05 08:39:16 +0530

    Add sorting function for 0s, 1s, and 2s
    Implemented the Dutch National Flag Algorithm to sort an array of 0s, 1s, and 2s in-place.

--- GFG_POTD/sort_0s_1s_2s.py
+++ GFG_POTD/sort_0s_1s_2s.py
@@ -1,1 +1,25 @@
+class Solution:
+    def sort012(self, arr):
+        """
+        Dutch National Flag Algorithm
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+        """
+
+        low = 0
+        mid = 0
+        high = len(arr) - 1
+
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+
+            elif arr[mid] == 1:
+                mid += 1
+
+            else:  # arr[mid] == 2
+                arr[mid], arr[high] = arr[high], arr[mid]
+                high -= 1
 

1511da4efba7e25c4915d7b28ad1a21230706321
sumathi255 <adimulamsumathi432@gmail.com>
2026-01-05 10:26:53 +0530

    Add insertion sort implementation
    Implemented the insertion sort algorithm to sort an array in ascending order.

--- Striver_DSA_Sheet/Sorting/insertion_sort.py
+++ Striver_DSA_Sheet/Sorting/insertion_sort.py
@@ -1,1 +1,25 @@
-Add Insertion Sort
+def insertion_sort(arr):
+    n = len(arr)
+
+    # Start from second element (first is already sorted)
+    for i in range(1, n):
+
+        key = arr[i]     # Element to be inserted
+        j = i - 1        # Index of previous element
+
+        # ASCENDING ORDER LOGIC:
+        # Shift elements that are greater than key to one position ahead
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]   # Shift right
+            j -= 1
+
+        # 🔽 DESCENDING ORDER LOGIC (ONLY COMMENT):
+        # while j >= 0 and arr[j] < key:
+        #     arr[j + 1] = arr[j]
+        #     j -= 1
+
+        # Insert key at correct position
+        arr[j + 1] = key
+
+    return arr
+
